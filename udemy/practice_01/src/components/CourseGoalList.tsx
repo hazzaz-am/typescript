2e3e985d6a6c5ecf3091760888fd272123285568
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-02-03 12:02:34 +0600

    completed project 1

--- udemy/practice_01/src/components/CourseGoalList.tsx
+++ udemy/practice_01/src/components/CourseGoalList.tsx
@@ -1,4 +1,6 @@
+import { type ReactNode } from "react";
 import { CourseGoalType } from "../App";
+import InfoBox from "../InfoBox";
 import CourseGoal from "./CourseGoal";
 
 type CourseGoalListProps = {
@@ -7,16 +9,39 @@ type CourseGoalListProps = {
 };
 
 const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
+	if (goals.length === 0) {
+		return (
+			<InfoBox mode="hint">
+				No course goals found. Add some by clicking the "Add a new goal" button
+				above.
+			</InfoBox>
+		);
+	}
+
+	let warningBox: ReactNode;
+
+	if (goals.length > 3) {
+		warningBox = (
+			<InfoBox mode="warning" severity="medium">
+				Warning: You have more than 3 goals. Consider breaking them down into
+				smaller, more manageable goals.
+			</InfoBox>
+		);
+	}
+
 	return (
-		<ul>
-			{goals.map((goal) => (
-				<li key={goal.id}>
-					<CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-						<p>{goal.description}</p>
-					</CourseGoal>
-				</li>
-			))}
-		</ul>
+		<>
+			{warningBox}
+			<ul>
+				{goals.map((goal) => (
+					<li key={goal.id}>
+						<CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+							<p>{goal.description}</p>
+						</CourseGoal>
+					</li>
+				))}
+			</ul>
+		</>
 	);
 };
 export default CourseGoalList;
