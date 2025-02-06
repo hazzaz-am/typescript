30e97ae4ca3f8f4884c94805a0285a0ab5fb6090
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-02-06 07:52:38 +0600

    add interval

--- udemy/react-timer/src/components/Timer.tsx
+++ udemy/react-timer/src/components/Timer.tsx
@@ -1,15 +1,25 @@
-import Container from './UI/Container.tsx';
+import { useState } from "react";
+import Container from "./UI/Container.tsx";
 
 type TimerProps = {
-  name: string;
-  duration: number
-}
+	name: string;
+	duration: number;
+};
 
-export default function Timer({name, duration}: TimerProps) {
-  return (
-    <Container as="article">
-      <h2>TODO: {name}</h2>
-      <p>{duration}</p>
-    </Container>
-  );
+export default function Timer({ name, duration }: TimerProps) {
+	const [remainingState, setRemainingState] = useState(duration * 1000);
+
+	setInterval(() => {
+		setRemainingState((prevTime) => prevTime - 50);
+	}, 50);
+
+	return (
+		<Container as="article">
+			<h2>TODO: {name}</h2>
+			<p>
+				<progress max={duration * 1000} value={remainingState} />
+			</p>
+			<p>{duration}</p>
+		</Container>
+	);
 }
